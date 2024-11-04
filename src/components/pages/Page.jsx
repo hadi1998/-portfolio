88ee6438164ca2d0900e42b067b87df3563d0c91
Hadi  Khaleghi <hadikhaleghi14@gmail.com>
2024-11-04 13:14:11 +0330

    slider updated

--- src/components/pages/Page.jsx
+++ src/components/pages/Page.jsx
@@ -4,13 +4,11 @@ const Page = (props) => {
   const { children, pageNumber, index, ...others } = props;
   return (
     <div
-      role="tabpanel"
+      role="switch"
       hidden={pageNumber !== index}
       id={`tabpanel-${index}`}
       aria-label={`sidebar-tab-${index}`}
       {...others}
-      disableSwipe 
-      disableTransition
     >
       {pageNumber === index && <Box sx={{ height: "100vh",width: "100%", overflow:"hidden" }}>{children}</Box>}
     </div>
